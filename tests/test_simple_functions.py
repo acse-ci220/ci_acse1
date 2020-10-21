3d4b55f87f09c0ed2b898ddd6e98f8ed91114e69
unknown <ci220@ic.ac.uk>
2020-10-21 14:37:57 +0100

    Flake8 issues debugging

--- tests/test_simple_functions.py
+++ tests/test_simple_functions.py
@@ -14,8 +14,9 @@ class TestSimpleFunctions(object):
     def test_my_add(self, iterable, expected):
         '''Test our add function'''
         isum = my_sum(iterable)
-        
+
         assert isum == expected
+
     @pytest.mark.parametrize('number, expected', [
         (5, 120),
         (3, 6),
